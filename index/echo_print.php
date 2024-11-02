4358f01cbe8bff70d7a7f798cb9b5c60f799357e
diatsilva007 <diatsilva007@gmail.com>
2024-11-02 12:00:59 -0300

    feat: Funcionamento do PHP um pouco mais a fundo (páginas estáticas x dinâmicas)

---
+++ index/echo_print.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+    <html lang="en" lang="pt-br">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php - Echo print</title>
+
+    </head>
+
+    <body>
+
+        <? 
+        echo 'Comando eco <br/>';   
+
+        echo print "Comando print <hr/>";
+        ?>
+
+    </body>
+
+    </html>
