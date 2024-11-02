d4f4d3fe3764ea88671aced896f8b4639a04085b
diatsilva007 <diatsilva007@gmail.com>
2024-11-01 23:03:15 -0300

    docs: servidor apache e pasta htdocs e suas dependências criadas e atualizadas

---
+++ index.php
@@ -0,0 +1,23 @@
+    <!DOCTYPE html>
+    <html lang="en" lang="pt-br">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Php</title>
+
+    </head>
+
+    <body>
+        
+        <?php
+
+          echo 'Utilizando a tag padrão'
+
+        ?>
+            <br>
+        <?= 'Utilizando a tag impressão' ?>
+
+    </body>
+
+    </html>
