33e9ab75be4bae4fb082eff70a23e41fa7acad8b
diatsilva007 <diatsilva007@gmail.com>
2024-11-02 22:25:50 -0300

    feat: <!-- Comentário html -->, // Comentário de uma linha, # Comentário de uma linha e /* Comentário de múltiplas linhas */

--- index/comentarios.php
+++ index/comentarios.php
@@ -10,11 +10,23 @@
 
     <body>
 
+        <!-- Comentário html -->
+
         <? 
 
+        // Comentário de uma linha
         echo 'Comando eco <br/>';   
 
+        # Comentário de uma linha
         echo print "Comando print <hr/>";
+
+        /* Comentário de múltiplas linhas
+        echo 'Comando eco <br/>';
+        echo 'Comando eco <br/>';
+        echo 'Comando eco <br/>';
+        Este tipo de comentário, permite que várias linhas
+        sejam comentadas ao mesmo tempo
+        */
         
         ?>
 
