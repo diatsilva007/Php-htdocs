4cd297a678a55f7dc7a2227bee868b24735280da
diatsilva007 <diatsilva007@gmail.com>
2024-11-02 23:00:53 -0300

    docs: Título da página atualizada

--- index/variaveis.php
+++ index/variaveis.php
@@ -4,7 +4,7 @@
 
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Php - Comenttários</title>
+        <title>Php - Variáveis</title>
 
     </head>
 
