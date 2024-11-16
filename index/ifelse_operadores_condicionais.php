9d37d8903753da1d7d70b66566b8cab45c184426
diatsilva007 <diatsilva007@gmail.com>
2024-11-15 22:22:20 -0300

    feat: if/else parte 2 - Praticando com operadores de comparação

---
+++ index/ifelse_operadores_condicionais.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - If/Else operadores condicionais</title>
+
+</head>
+
+<body>
+
+    <!-- Condições If / Else -->
+
+    <?php
+
+    // Operadores de comparação
+
+    //==
+    //===
+    //!= ou <>
+    //!==
+    //<
+    //<=
+    //>
+    //>=
+
+    if (10 >= 5) {
+        echo 'Verdadeiro'; //true
+        
+    } else {
+        echo 'Falso'; //false
+    }
+
+    ?>
+
+</body>
+
+</html>
