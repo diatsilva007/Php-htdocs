d84302ce32c1f0464fcae5d9cb51596e0b7f0e2d
diatsilva007 <diatsilva007@gmail.com>
2024-11-16 23:20:56 -0300

    feat: If/else parte 3 - praticando com operadores lógicos

---
+++ index/ifelse_operadores_logicos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - If/Else operadores lógicos</title>
+
+</head>
+
+<body>
+
+    <!-- Condições If / Else -->
+
+    <?php
+
+    // Operadores de comparação/condicionais.
+
+    //==
+    //===
+    //!= ou <>
+    //!==
+    //<
+    //<=
+    //>
+    //>=
+
+    //Operadores lógicos
+    //E: && ou AND -> retorna verdadeiro se todos os resultados da expressões forem verdadeiros.
+    //OU: || ou OR -> retorna verdadeiro se pelo menos um dos resultados das expressões forem verdadeiros.
+    //XOR: XOR -> retorna verdadeiro se uma das expressões for verdadeira e a outra falsa, ou vice-versa.
+    //! -> inverte o resultado retornado pela expressão.
+
+    //() estabelecer precedência
+    // (v e v) = v ou f
+    if ((22  == 22 && 3 == 3) || 5 != 5) {
+        echo 'Verdadeiro'; //true
+    } else {
+        echo 'Falso'; //false
+    }
+
+    ?>
+
+</body>
+
+</html>
