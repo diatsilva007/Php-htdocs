789c59d2861c03e3ed255f2ab13819cb8586c721
diatsilva007 <diatsilva007@gmail.com>
2024-11-30 23:37:37 -0300

    feat: lógica para operadores ternários

--- index/operador_ternario.php
+++ index/operador_ternario.php
@@ -17,11 +17,18 @@
 
     //
     $usuario_possui_cartao_loja = true;
-    $valor_compra = 50;
+    $valor_compra = 500;
 
     $valor_frete = 50;
     $recebeu_desconto_frete = true;
 
+    $valor_frete_aux = $usuario_possui_cartao_loja && $valor_compra >= 400 ? 0 : ($usuario_possui_cartao_loja && $valor_compra >= 300 ? 10 : ($usuario_possui_cartao_loja && $valor_compra >= 100 ? 25 : $valor_frete) );
+
+    $recebeu_desconto_frete = $valor_frete != $valor_frete_aux ? true : false;
+
+    $valor_frete = $valor_frete_aux;
+
+    /*
     if ($usuario_possui_cartao_loja && $valor_compra >= 400) {
         $valor_frete = 0;
     } else if ($usuario_possui_cartao_loja && $valor_compra >= 300) {
@@ -31,46 +38,18 @@
     } else {
         $recebeu_desconto_frete = false;
     }
+    */
 
     ?>
 
     <h1>Detalhes do pedido</h1>
 
     <p>Possui cartão da loja? <?= $usuario_possui_cartao_loja ? 'SIM' : 'NÃO'; ?>
-        <?php
-
-        // <condição> ? true: false
-
-        /*
-        if ($usuario_possui_cartao_loja) {
-            echo 'Sim';
-        } else {
-            echo 'Não';
-        }
-        */
-
-        ?>
-
     </p>
 
     <p>Valor da compra: <?= $valor_compra ?></p>
 
-    <p>Recebeu desconto do frete?
-        <?php
-
-        $teste =  $recebeu_desconto_frete ? 'SIM' : 'NÃO';
-
-        echo $teste;
-
-        /*
-        if ($recebeu_desconto_frete) {
-            echo 'Sim';
-        } else {
-            echo 'Não';
-        }
-        */
-
-        ?>
+    <p>Recebeu desconto no frete? <?= $teste =  $recebeu_desconto_frete ? 'SIM' : 'NÃO'; ?>
     </p>
 
     <p>Valor do frete: <?= $valor_frete ?></p>
