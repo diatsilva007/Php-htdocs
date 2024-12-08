1ba965c0ae5008552ef31f5f57597439f9bdf473
diatsilva007 <diatsilva007@gmail.com>
2024-12-07 23:48:47 -0300

    feat: Switch na prática

---
+++ index/switch.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Switch na prática</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        switch (2 -2) {
+            case 'value':
+                # code...
+                break;
+
+            default:
+                # code...
+                break;
+            }
+
+    ?>
+
+</body>
+
+</html>
