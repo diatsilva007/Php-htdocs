0b23b72b4bd62cf6c61b17ba954eb1eddd891562
diatsilva007 <diatsilva007@gmail.com>
2024-12-09 22:10:34 -0300

    feat: Praticando com operadores aritméticos

--- index/operadores_aritmeticos.php
+++ index/operadores_aritmeticos.php
@@ -19,6 +19,14 @@
     $num2 = 4;
 
     echo "A soma entre $num1 e $num2 é: " . ($num1 + $num2);
+    echo "<br>";
+    echo "A subtração entre $num1 e $num2 é: " . ($num1 - $num2);
+    echo "<br>";
+    echo "A multiplicação entre $num1 e $num2 é: " . ($num1 * $num2);
+    echo "<br>";
+    echo "A divisão entre $num1 e $num2 é: " . ($num1 / $num2);
+    echo "<br>";
+    echo "O resto da divisão entre $num1 e $num2 é: ". ($num1 % $num2);
 
     ?>
 
