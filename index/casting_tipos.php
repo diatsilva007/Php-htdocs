1baa5e074e8f95c432fe7dc7d6f555b64260bfbe
diatsilva007 <diatsilva007@gmail.com>
2024-12-09 21:50:49 -0300

    feat: Casting de tipos com (int), (bool), (float) e (string)

--- index/casting_tipos.php
+++ index/casting_tipos.php
@@ -16,16 +16,26 @@
     // Casting de tipos
 
     // gettype() => retorna o tipo da variável
-    $valor = 15.35;
+    $valor = true;
     //$valor2 = (float) $valor; // retorna o tipo da variável: floa, double.
     //$valor2 = (string) $valor;
 
     //valor2 = (int) $valor; // retorna o tipo da variável: int.
+    //$valor2 = (string) $valor;
+
+    //
+    //$valor2 = (boolean) $valor; // retorna o tipo da variável: bool ou boolean
+
+    //$valor2 = (int) $valor;
     $valor2 = (string) $valor;
+    $valor3 = (int) $valor2;
+    
 
     echo $valor . ' ' . gettype($valor);
     echo '<br>';
     echo $valor2 . ' ' . gettype($valor2);
+    echo '<br>';
+    echo $valor3 . ' ' . gettype($valor3);
 
     ?>
 
