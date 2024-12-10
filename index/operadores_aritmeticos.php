afe805c06befa5f395391b04da509602a72a2168
diatsilva007 <diatsilva007@gmail.com>
2024-12-09 22:07:11 -0300

    feat: Praticando com operadores aritméticos

---
+++ index/operadores_aritmeticos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Operadores aritméticos</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    // Operadores aritméticos
+
+    $num1 = 13;
+    $num2 = 4;
+
+    echo "A soma entre $num1 e $num2 é: " . ($num1 + $num2);
+
+    ?>
+
+</body>
+
+</html>
