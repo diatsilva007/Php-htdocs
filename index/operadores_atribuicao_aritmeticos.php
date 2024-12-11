550971bce1eb6035cdc9355df1d6db8c3aa1af91
diatsilva007 <diatsilva007@gmail.com>
2024-12-10 22:17:41 -0300

    feat: Operações aritméticas na atribuição de valores

---
+++ index/operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Operadores de atribuição aritméticos</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    // Operadores de atribuição aritméticos
+
+    ?>
+
+</body>
+
+</html>
