81a22c57b6cb890b1014d79d63576498f12a8c87
diatsilva007 <diatsilva007@gmail.com>
2024-12-12 22:25:06 -0300

    feat: Praticando com operadores de incremento/decremento

---
+++ index/operadores_incremento_decremento.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Operadores de incremento e decremento</title>
+
+</head>
+
+<body>
+
+    <h3>Pós-incremento</h3>
+
+    <?php
+
+    // Operadores de incremento e decremento
+
+    $a = 7; // Incre
+
+    echo "O valor contido em a é: $a <br/>";
+    
+
+    ?>
+
+</body>
+
+</html>
