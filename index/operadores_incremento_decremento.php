ccc7916618465d600f7c68a93eca45b6e41251fb
diatsilva007 <diatsilva007@gmail.com>
2024-12-12 22:34:51 -0300

    feat: Praticando com operadores de incremento/decremento

--- index/operadores_incremento_decremento.php
+++ index/operadores_incremento_decremento.php
@@ -20,7 +20,54 @@
     $a = 7; // Incre
 
     echo "O valor contido em a é: $a <br/>";
-    
+    echo 'O valor contido em a após o incremento é: ' . $a++ . ' <br/>';
+    echo "O valor atualizado é: $a "; // Increment
+
+
+    ?>
+
+    <h3>Pré-incremento</h3>
+
+    <?php
+
+    // Operadores de incremento e decremento
+
+    $a = 7; // Incre
+
+    echo "O valor contido em a é: $a <br/>";
+    echo 'O valor contido em a pré o incremento é: ' . ++$a . ' <br/>'; // Pre increment
+    echo "O valor atualizado é: $a "; // Pre increment
+
+
+    ?>
+
+    <h3>Pós-decremento</h3>
+
+    <?php
+
+    // Operadores de incremento e decremento
+
+    $a = 7; // Incre
+
+    echo "O valor contido em a é: $a <br/>";
+    echo 'O valor contido em a após o decremento é: ' . $a-- . ' <br/>';
+    echo "O valor atualizado é: $a "; // Increment
+
+
+    ?>
+
+    <h3>Pré-decremento</h3>
+
+    <?php
+
+    // Operadores de incremento e decremento
+
+    $a = 7; // Incre
+
+    echo "O valor contido em a é: $a <br/>";
+    echo 'O valor contido em a após o pré deremento é: ' . --$a . ' <br/>';
+    echo "O valor atualizado é: $a "; // Increment
+
 
     ?>
 
