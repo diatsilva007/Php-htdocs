5cd3791c2dee275d32c2e00eeb9decdc9ba3c2b5
diatsilva007 <diatsilva007@gmail.com>
2025-01-23 21:28:23 -0300

    feat: Adicionar página de boas-vindas em PHP

---
+++ index/funcoes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Operadores de incremento e decremento</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        function exibirBoasVindas() {
+            echo "Seja bem-vindo ao curso de PHP! <br/>";
+        }
+
+        exibirBoasVindas();
+
+    ?>
+
+</body>
+
+</html>
