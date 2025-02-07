1e7f31950b087f00aca246f8992a63d6e4870176
diatsilva007 <diatsilva007@gmail.com>
2025-02-06 22:34:38 -0300

    feat: Adicionar página de funções matemáticas em PHP

---
+++ index/funcoes_matematicas.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Funções matemáticas</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        // Funções matemáticas
+
+        $num = 7.5;
+
+        //echo ceil($num) // Arredonda para cima
+        //echo floor($num) // Arredonda para baixo
+        //echo round($num) // Arredonda com base na fração
+        //echo rand(10, 20); // Gera um valor aleatório 0 até randmax
+        //echo '<br/>' .getrandmax()
+        echo sqrt(9) // Retona a raiz quadrada
+
+    ?>
+
+</body>
+
+</html>
