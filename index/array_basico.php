cdaf01f80de26d02398c2ff3667fec4fda3b74ce
diatsilva007 <diatsilva007@gmail.com>
2025-02-09 23:10:43 -0300

    feat: Adicionar página de exemplo com array básico em PHP

---
+++ index/array_basico.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Array Básico</title>
+
+</head>
+
+<body>
+
+    <?php
+
+    // Array básico    
+
+    //sequenciais (numéricos)
+    $lista_frutas = array('Banana', 'Maçã', 'Morango', 'Uva', 'Abacaxi');
+
+    echo '<pre>';
+    var_dump($lista_frutas);
+    echo '</pre>';
+    echo '<hr />';
+    echo '<pre>';
+    print_r($lista_frutas);
+    echo '</pre>';
+
+    //associativos
+
+    ?>
+
+</body>
+
+</html>
