f01c8cc7cbe7676cfcd6ea7f99449a1b8932cbf1
diatsilva007 <diatsilva007@gmail.com>
2025-02-09 23:23:07 -0300

    feat: Atualizar array de frutas para sintaxe moderna e adicionar novo item

--- index/array_basico.php
+++ index/array_basico.php
@@ -16,8 +16,13 @@
     // Array básico    
 
     //sequenciais (numéricos)
-    $lista_frutas = array('Banana', 'Maçã', 'Morango', 'Uva', 'Abacaxi');
+    //$lista_frutas = array('Banana', 'Maçã', 'Morango', 'Uva', 'Abacaxi');
 
+    $lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva', 'Abacaxi'];
+
+    $lista_frutas[] = 'Mamão';
+
+    /*
     echo '<pre>';
     var_dump($lista_frutas);
     echo '</pre>';
@@ -25,8 +30,26 @@
     echo '<pre>';
     print_r($lista_frutas);
     echo '</pre>';
+    */
+
+    //echo $lista_frutas[4]; //retorna apenas o índice abacaxi
 
     //associativos
+    $lista_frutas = [
+        'a' => 'Banana',
+        'b' => 'Maçã',
+        'c' => 'Morango',
+        '1' => 'Uva',
+        '2' => 'Abacaxi'
+    ];
+
+    $lista_frutas['w'] = 'Mamão';
+
+    echo '<pre>';
+    var_dump($lista_frutas);
+    echo '</pre>';
+
+    echo $lista_frutas['w']; //retorna apenas o índice mamão
 
     ?>
 
