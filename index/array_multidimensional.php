e1f8763504ba66d7985b99b36f3e761375a02613
diatsilva007 <diatsilva007@gmail.com>
2025-02-10 22:21:32 -0300

    feat: Adicionar página inicial para exemplo de array multidimensional em PHP

---
+++ index/array_multidimensional.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Array Multidimensional</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        
+
+    ?>
+
+</body>
+
+</html>
