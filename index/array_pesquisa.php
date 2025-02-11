4304c9351b9524a43e24b3287024435e5dd42eeb
diatsilva007 <diatsilva007@gmail.com>
2025-02-10 22:48:16 -0300

    feat: Implementar pesquisa de valor em array com array_search e exibir resultado

--- index/array_pesquisa.php
+++ index/array_pesquisa.php
@@ -13,11 +13,24 @@
 
     <?php
 
-        //in_array();
-        //array_search();
+    //in_array(); -> true ou false para a existência do que está sendo procurado
+    //array_search(); -> retorna o índice do valor pesquisado, caso ele exista
 
-        $lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
-    
+    $lista_frutas = [1 => 'Banana', 2 => 'Maçã', 3 => 'Morango', 4 => 'Uva'];
+
+    echo '<pre>';
+    print_r($lista_frutas);
+    echo '</pre>';
+
+    //$existe = in_array('Uva', $lista_frutas);
+    $existe = array_search('Uva', $lista_frutas);
+    //null
+
+    if ($existe != null) { //true / false
+        echo 'Sim, o valor pesquisado existe no array.';
+    } else {
+        echo 'Não, o valor pesquisado não existe no array.';
+    }
 
     ?>
 
