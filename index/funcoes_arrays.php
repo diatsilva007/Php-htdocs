a8f7dea210db75b07b13a9a4c5cf74dc6644218b
diatsilva007 <diatsilva007@gmail.com>
2025-02-11 11:28:43 -0300

    feat: Corrigir título da página e adicionar verificação de array em PHP

--- index/funcoes_arrays.php
+++ index/funcoes_arrays.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php - Funcções arrays</title>
+    <title>Php - Funções arrays</title>
 
 </head>
 
@@ -14,6 +14,14 @@
     <?php
 
     // Funções para arrays
+    $array = array('Notebook', 'Teclado', 'Mouse', 'HD Externo'); //array simples
+    $retorno = is_array($array); //verifica se é um array
+
+    if ($retorno) {
+        echo 'Sim, é um array'; //retorna true
+    } else {
+        echo 'Não, é um array'; //retorna false
+    }
 
     ?>
 
