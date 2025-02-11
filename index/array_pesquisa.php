864e68dd64c7b25f065e30b314f864eae2fb860e
diatsilva007 <diatsilva007@gmail.com>
2025-02-10 22:35:07 -0300

    feat: Adicionar página de pesquisa de arrays em PHP

---
+++ index/array_pesquisa.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Array pesquisas</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        //in_array();
+        //array_search();
+
+        $lista_frutas = ['Banana', 'Maçã', 'Morango', 'Uva'];
+    
+
+    ?>
+
+</body>
+
+</html>
