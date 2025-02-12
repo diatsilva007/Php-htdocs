34b4c74fb0ba7b135f0ce47089a1372884c1f6a3
diatsilva007 <diatsilva007@gmail.com>
2025-02-11 23:02:21 -0300

    feat: Adicionar página para demonstração de loops for em PHP

---
+++ index/loops_for.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Loops for</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        // Loops for
+        
+
+    ?>
+
+</body>
+
+</html>
