e4fabfad987783583a151037b75ec4db157f812a
diatsilva007 <diatsilva007@gmail.com>
2025-02-12 22:35:05 -0300

    feat: Adicionar estrutura básica de página para prática de loops em PHP

---
+++ index/loops_pratica_1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php - Loops prática</title>
+
+</head>
+
+<body>
+
+    <?php
+
+        // Loops na prática
+        
+
+    ?>
+
+</body>
+
+</html>
