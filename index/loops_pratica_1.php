c345b1c7bcbba2f0a24da1d8554f9cdfaadc2188
diatsilva007 <diatsilva007@gmail.com>
2025-02-12 23:53:37 -0300

    feat: Corrigir título da página para "Php - Loops na prática"

--- index/loops_pratica_1.php
+++ index/loops_pratica_1.php
@@ -5,7 +5,7 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php - Loops prática</title>
+    <title>Php - Loops na prática</title>
 
 </head>
 
