4e51276d18b921e9ca40394425fea072b8368468
diatsilva007 <diatsilva007@gmail.com>
2025-02-12 22:52:49 -0300

    feat: Adicionar exemplo de uso de loop while com registros em PHP

--- index/loops_pratica_1.php
+++ index/loops_pratica_1.php
@@ -13,8 +13,21 @@
 
     <?php
 
-        // Loops na prática
-        
+    // Loops na prática
+    $registros = array(
+        array('titulo' => 'Título notícia 1', 'conteudo' => 'Conteúdo notícia 1'),
+        array('titulo' => 'Título notícia 2', 'conteudo' => 'Conteúdo notícia 2'),
+        array('titulo' => 'Título notícia 3', 'conteudo' => 'Conteúdo notícia 3')
+    );
+
+    $idx = 0;
+    while ($idx < 3) {
+
+        echo $registros[$idx];
+        echo '<hr/>';
+
+        $idx++;
+    }
 
     ?>
 
