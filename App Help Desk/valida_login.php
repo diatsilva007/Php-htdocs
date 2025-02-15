0bb1d65f545f9cdb1f811f79024c1a7b6f61526f
diatsilva007 <diatsilva007@gmail.com>
2025-02-14 23:25:22 -0300

    feat: Adicionar suporte à validação de dados do formulário usando método POST

--- App Help Desk/valida_login.php
+++ App Help Desk/valida_login.php
@@ -1,5 +1,6 @@
 <?php
 
+    /*
     print_r($_GET);
 
     echo '<br />';
@@ -7,5 +8,13 @@
     echo $_GET['email'];
     echo '<br />';
     echo $_GET['senha']
+    */
 
+    print_r($_POST);
+    
+    echo '<br />';
+
+    echo $_POST['email'];
+    echo '<br />';
+    echo $_POST['senha']
 ?>
