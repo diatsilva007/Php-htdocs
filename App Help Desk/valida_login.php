41333c90028838cc325ff6c0846799ab3585e1f6
diatsilva007 <diatsilva007@gmail.com>
2025-02-15 23:59:19 -0300

    feat: Implementar autenticação de usuários no sistema de login

--- App Help Desk/valida_login.php
+++ App Help Desk/valida_login.php
@@ -1,6 +1,34 @@
 <?php
 
-    /*
+// variável que verifica se a  autenticação foi realizada
+$usuario_autenticado = false;
+
+// usuários do sistema
+$usuarios_app = array(
+    array('email' => 'admin@example.com', 'senha' => '123456'),
+    array('email' => 'user@example.com', 'senha' => 'abcd'),
+);
+
+/*
+echo '<pre>';
+print_r($usuarios_app);
+echo '</pre>';
+*/
+
+foreach ($usuarios_app as $user) {
+
+    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+        $usuario_autenticado = true;
+    }
+}
+
+if ($usuario_autenticado) {
+    echo 'Login realizado com sucesso!';
+} else {
+    header('Location: index.php?login=erro');
+}
+
+/*
     print_r($_GET);
 
     echo '<br />';
@@ -9,13 +37,14 @@
     echo '<br />';
     echo $_GET['senha']
     */
+/*
+print_r($_POST);
 
-    print_r($_POST);
-    
-    echo '<br />';
+echo '<br />';
 
-    echo $_POST['email'];
-    echo '<br />';
-    echo $_POST['senha']
-    
+echo $_POST['email'];
+echo '<br />';
+echo $_POST['senha']
+*/
+
 ?>
