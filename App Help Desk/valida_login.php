282b150bd78b852682984b98cc9902eba7df8937
diatsilva007 <diatsilva007@gmail.com>
2025-02-18 22:45:57 -0300

    feat: Adicionar verificação de sessão e redirecionamento para login na página inicial

--- App Help Desk/valida_login.php
+++ App Help Desk/valida_login.php
@@ -1,50 +1,58 @@
 <?php
 
-// variável que verifica se a  autenticação foi realizada
+session_start();
+$_SESSION['x'] = 'Oi, eu sou um valor de sessão!';
+print_r($_SESSION);
+echo '<hr/>';
+print_r($_SESSION['y']);
+
+
+//VARIAVEL QUE VERIFICA SE A AUTENTICAÇÃO FOI REALIZADA
 $usuario_autenticado = false;
 
-// usuários do sistema
+//USUARIOS DO SISTEMA
 $usuarios_app = array(
-    array('email' => 'admin@example.com', 'senha' => '123456'),
-    array('email' => 'user@example.com', 'senha' => 'abcd'),
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
 );
-
 /*
+
 echo '<pre>';
 print_r($usuarios_app);
 echo '</pre>';
+
 */
 
-foreach ($usuarios_app as $user) {
-
-    if ($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+foreach($usuarios_app as $user){
+    /*
+    echo 'Usuario app: ' . $user['email'] . '/' . $user['senha'];
+    echo '<br />';
+    echo 'Usuario form: ' . $_POST['email'] . '/' . $_POST['senha'];
+    echo '<hr />';
+    */
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
         $usuario_autenticado = true;
     }
 }
 
-if ($usuario_autenticado) {
-    echo 'Login realizado com sucesso!';
-} else {
-    header('Location: index.php?login=erro');
-}
+    if($usuario_autenticado){
+        echo 'Usuário autenticado.';
+
+        $_SESSION['autenticado'] = 'SIM';
+    }else{  
+        $_SESSION['autenticado'] = 'NÃO';
+        header('Location: index.php?login=erro');
+      }
+
 
 /*
-    print_r($_GET);
+print_r($_GET)
 
-    echo '<br />';
-
-    echo $_GET['email'];
-    echo '<br />';
-    echo $_GET['senha']
-    */
-/*
+echo '<br />'
+echo $_GET['email']
+echo '<br />'
+echo $_GET['senha']
 print_r($_POST);
-
-echo '<br />';
-
-echo $_POST['email'];
-echo '<br />';
-echo $_POST['senha']
 */
 
 ?>
