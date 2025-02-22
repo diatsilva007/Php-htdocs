4e45baa8aa80dddb73b2541e9a2604049e086833
diatsilva007 <diatsilva007@gmail.com>
2025-02-22 08:21:14 -0300

    refactor: Melhorar a formatação e a legibilidade das classes Funcionario, Carro, Moto e Veiculo

--- php_oo/oo_pilar_heranca.php
+++ php_oo/oo_pilar_heranca.php
@@ -1,71 +1,81 @@
 <?php
 
 
-	class Carro extends Veiculo {
-		public $teto_solar = true;
+class Carro extends Veiculo
+{
+	public $teto_solar = true;
 
-		function __construct($placa, $cor) {
-			$this->placa = $placa;
-			$this->cor = $cor;
-		}
-
-		function abrirTetoSolar() {
-			echo 'Abrir teto solar';
-		}
-
-		function alterarPosicaoVolante() {
-			echo 'Alterar posição volante';
-		}
+	function __construct($placa, $cor)
+	{
+		$this->placa = $placa;
+		$this->cor = $cor;
 	}
 
-	class Moto extends Veiculo {
-		public $contraPesoGuidao = true;
-
-		function __construct($placa, $cor) {
-			$this->placa = $placa;
-			$this->cor = $cor;
-		}
-
-		function empinar() {
-			echo 'Empinar';
-		}
+	function abrirTetoSolar()
+	{
+		echo 'Abrir teto solar';
 	}
 
-	class Veiculo {
-		public $placa = null;
-		public $cor = null;
+	function alterarPosicaoVolante()
+	{
+		echo 'Alterar posição volante';
+	}
+}
 
-		function acelerar() {
-			echo 'Acelerar';
-		}
+class Moto extends Veiculo
+{
+	public $contraPesoGuidao = true;
 
-		function freiar() {
-			echo 'Freiar';
-		}
+	function __construct($placa, $cor)
+	{
+		$this->placa = $placa;
+		$this->cor = $cor;
 	}
 
-	$carro = new Carro('ABC1234', 'Branco');
-	$moto = new Moto('DEF1122', 'Preto');
+	function empinar()
+	{
+		echo 'Empinar';
+	}
+}
 
-	echo '<pre>';
-	print_r($carro);
-	echo '<br /><br />';
-	print_r($moto);
-	echo '</pre>';
-	echo '<hr />';
+class Veiculo
+{
+	public $placa = null;
+	public $cor = null;
 
-	$carro->abrirTetoSolar();
-	echo '<br />';
-	$carro->acelerar();
-	echo '<br />';
-	$carro->freiar();
+	function acelerar()
+	{
+		echo 'Acelerar';
+	}
 
-	echo '<hr />';
+	function freiar()
+	{
+		echo 'Freiar';
+	}
+}
 
-	$moto->empinar();
-	echo '<br />';
-	$moto->acelerar();
-	echo '<br />';
-	$moto->freiar();
+$carro = new Carro('ABC1234', 'Branco');
+$moto = new Moto('DEF1122', 'Preto');
+
+echo '<pre>';
+print_r($carro);
+echo '<br /><br />';
+print_r($moto);
+echo '</pre>';
+echo '<hr />';
+
+$carro->abrirTetoSolar();
+echo '<br />';
+$carro->acelerar();
+echo '<br />';
+$carro->freiar();
+
+echo '<hr />';
+
+$moto->empinar();
+echo '<br />';
+$moto->acelerar();
+echo '<br />';
+$moto->freiar();
 
 ?>
