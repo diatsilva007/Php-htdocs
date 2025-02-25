4ef8cbf2159155bf7decc333504698ee34fd1428
diatsilva007 <diatsilva007@gmail.com>
2025-02-24 23:24:41 -0300

    refactor: Melhorar a formatação e a legibilidade das classes e interfaces de exemplo

--- php_oo/interfaces.php
+++ php_oo/interfaces.php
@@ -1,130 +1,103 @@
 <?php
 
-interface EquipamentoEletronicoInterface
-{
-	public function ligar();
-	public function desligar();
-}
-
-//-----------------------------
-
-class Geladeira implements EquipamentoEletronicoInterface
-{
-	public function abrirPorta()
-	{
-		echo 'Abrir a porta';
+	interface EquipamentoEletronicoInterface {
+		public function ligar();
+		public function desligar();
 	}
 
-	public function ligar()
-	{
-		echo 'Ligar';
+	//-----------------------------
+
+	class Geladeira implements EquipamentoEletronicoInterface {
+		public function abrirPorta() {
+			echo 'Abrir a porta';
+		}
+
+		public function ligar(){
+			echo 'Ligar';
+		}
+
+		public function desligar() {
+			echo 'Desligar';
+		}
 	}
 
-	public function desligar()
-	{
-		echo 'Desligar';
-	}
-}
 
+	class TV implements EquipamentoEletronicoInterface {
+		public function trocarCanal() {
+			echo 'Trocar canal';
+		}
 
-class TV implements EquipamentoEletronicoInterface
-{
-	public function trocarCanal()
-	{
-		echo 'Trocar canal';
-	}
+		public function ligar() {
+			echo 'Ligar';
+		}
 
-	public function ligar()
-	{
-		echo 'Ligar';
+		public function desligar() {
+			echo 'Desligar';
+		}
 	}
 
-	public function desligar()
-	{
-		echo 'Desligar';
-	}
-}
+	$x = new Geladeira();
+	$y = new TV();
+	
 
-$x = new Geladeira();
-$y = new TV();
+	//-------------------------
 
-
-//-------------------------
-
-interface MamiferoInterface
-{
-	public function respirar();
-}
-
-interface TerrestreInterface
-{
-	public function andar();
-}
-
-interface AquaticoInterface
-{
-	public function nadar();
-}
-
-class Humano implements MamiferoInterface, TerrestreInterface
-{
-	public function andar()
-	{
-		echo 'Andar';
+	interface MamiferoInterface {
+		public function respirar();
 	}
 
-	public function respirar()
-	{
-		echo 'Respirar';
+	interface TerrestreInterface {
+		public function andar();
 	}
 
-	public function conversar()
-	{
-		echo 'Conversar';
-	}
-}
-
-class Baleia implements MamiferoInterface, AquaticoInterface
-{
-	public function respirar()
-	{
-		echo 'Respirar';
+	interface AquaticoInterface {
+		public function nadar();
 	}
 
-	public function nadar()
-	{
-		echo 'Nadar';
+	class Humano implements MamiferoInterface, TerrestreInterface {
+		public function andar() {
+			echo 'Andar';
+		}
+
+		public function respirar() {
+			echo 'Respirar';
+		}
+
+		public function conversar() {
+			echo 'Conversar';
+		}
 	}
 
-	protected function esguichar()
-	{
-		echo 'Esguichar';
-	}
-}
+	class Baleia implements MamiferoInterface, AquaticoInterface {
+		public function respirar() {
+			echo 'Respirar';
+		}
 
-//-----------------------------
+		public function nadar() {
+			echo 'Nadar';
+		}
 
-interface AnimalInterface
-{
-	public function comer();
-}
-
-interface AveInterface extends AnimalInterface
-{
-	public function voar();
-}
-
-class Papagaio implements AveInterface
-{
-	public function voar()
-	{
-		echo 'Voar';
+		protected function esguichar() {
+			echo 'Esguichar';
+		}
 	}
 
-	public function comer()
-	{
-		echo 'Comer';
-	}
-}
+	//-----------------------------
 
-?>
+	interface AnimalInterface {
+		public function comer();
+	}
+
+	interface AveInterface extends AnimalInterface {
+		public function voar();
+	}
+
+	class Papagaio implements AveInterface {
+		public function voar() {
+			echo 'Voar';
+		}
+
+		public function comer() {
+			echo 'Comer';
+		}
+	}
