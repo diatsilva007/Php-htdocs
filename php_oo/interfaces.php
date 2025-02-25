4b3b3ee0faf1e41d46483ca810a68b69c79d8893
diatsilva007 <diatsilva007@gmail.com>
2025-02-24 22:56:03 -0300

    refactor: Melhorar a implementação das interfaces e classes de equipamentos eletrônicos e mamíferos

--- php_oo/interfaces.php
+++ php_oo/interfaces.php
@@ -1,103 +1,130 @@
 <?php
 
-	interface EquipamentoEletronicoInterface {
-		public function ligar();
-		public function desligar();
+interface EquipamentoEletronicoInterface
+{
+	public function ligar();
+	public function desligar();
+}
+
+//-----------------------------
+
+class Geladeira implements EquipamentoEletronicoInterface
+{
+	public function abrirPorta()
+	{
+		echo 'Abrir a porta';
 	}
 
-	//-----------------------------
-
-	class Geladeira implements EquipamentoEletronicoInterface {
-		public function abrirPorta() {
-			echo 'Abrir a porta';
-		}
-
-		public function ligar(){
-			echo 'Ligar';
-		}
-
-		public function desligar() {
-			echo 'Desligar';
-		}
+	public function ligar()
+	{
+		echo 'Ligar';
 	}
 
+	public function desligar()
+	{
+		echo 'Desligar';
+	}
+}
 
-	class TV implements EquipamentoEletronicoInterface {
-		public function trocarCanal() {
-			echo 'Trocar canal';
-		}
 
-		public function ligar() {
-			echo 'Ligar';
-		}
-
-		public function desligar() {
-			echo 'Desligar';
-		}
+class TV implements EquipamentoEletronicoInterface
+{
+	public function trocarCanal()
+	{
+		echo 'Trocar canal';
 	}
 
-	$x = new Geladeira();
-	$y = new TV();
-	
-
-	//-------------------------
-
-	interface MamiferoInterface {
-		public function respirar();
+	public function ligar()
+	{
+		echo 'Ligar';
 	}
 
-	interface TerrestreInterface {
-		public function andar();
+	public function desligar()
+	{
+		echo 'Desligar';
+	}
+}
+
+$x = new Geladeira();
+$y = new TV();
+
+
+//-------------------------
+
+interface MamiferoInterface
+{
+	public function respirar();
+}
+
+interface TerrestreInterface
+{
+	public function andar();
+}
+
+interface AquaticoInterface
+{
+	public function nadar();
+}
+
+class Humano implements MamiferoInterface, TerrestreInterface
+{
+	public function andar()
+	{
+		echo 'Andar';
 	}
 
-	interface AquaticoInterface {
-		public function nadar();
+	public function respirar()
+	{
+		echo 'Respirar';
 	}
 
-	class Humano implements MamiferoInterface, TerrestreInterface {
-		public function andar() {
-			echo 'Andar';
-		}
+	public function conversar()
+	{
+		echo 'Conversar';
+	}
+}
 
-		public function respirar() {
-			echo 'Respirar';
-		}
-
-		public function conversar() {
-			echo 'Conversar';
-		}
+class Baleia implements MamiferoInterface, AquaticoInterface
+{
+	public function respirar()
+	{
+		echo 'Respirar';
 	}
 
-	class Baleia implements MamiferoInterface, AquaticoInterface {
-		public function respirar() {
-			echo 'Respirar';
-		}
-
-		public function nadar() {
-			echo 'Nadar';
-		}
-
-		protected function esguichar() {
-			echo 'Esguichar';
-		}
+	public function nadar()
+	{
+		echo 'Nadar';
 	}
 
-	//-----------------------------
+	protected function esguichar()
+	{
+		echo 'Esguichar';
+	}
+}
 
-	interface AnimalInterface {
-		public function comer();
+//-----------------------------
+
+interface AnimalInterface
+{
+	public function comer();
+}
+
+interface AveInterface extends AnimalInterface
+{
+	public function voar();
+}
+
+class Papagaio implements AveInterface
+{
+	public function voar()
+	{
+		echo 'Voar';
 	}
 
-	interface AveInterface extends AnimalInterface {
-		public function voar();
+	public function comer()
+	{
+		echo 'Comer';
 	}
+}
 
-	class Papagaio implements AveInterface {
-		public function voar() {
-			echo 'Voar';
-		}
-
-		public function comer() {
-			echo 'Comer';
-		}
-	}
+?>
