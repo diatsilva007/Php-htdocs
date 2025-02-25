aa0b8741146f9014cd53e0cc6ced4a9b2a1931c2
diatsilva007 <diatsilva007@gmail.com>
2025-02-24 22:54:00 -0300

    refactor: Melhorar a estrutura e a legibilidade da classe Exemplo

--- php_oo/atributos_e_metodos_estaticos.php
+++ php_oo/atributos_e_metodos_estaticos.php
@@ -1,28 +1,24 @@
 <?php
-class Exemplo
-{
-    public static $atributo1 = 'Eu sou um atributo static';
-    public $atributo2 = 'Eu sou atributo normal';
 
-    public static function metodo1()
-    {
-        // echo $this->atributo2; //dará erro!
-        echo 'Eu sou um método estático';
-    }
+	class Exemplo {
+		public static $atributo1 = 'Eu sou um atributo estático';
+		public $atributo2 = 'Eu sou um atributo normal';
 
-    public function metodo2()
-    {
-        echo 'Eu sou um método normal';
-    }
-}
+		public static function metodo1() {
+			echo 'Eu sou um método estático';
+		}
 
-//$x = new Exemplo();
-/* Atributos e métodos estáticos podem ser acessados diretamente, sem precisar instanciar */
-/* echo Exemplo::$atributo1;
-    echo '<br />';
-    echo Exemplo::metodo1(); */
-//echo Exemplo::$metodo2(); //Atualização-> Não funciona mais.
-//echo $x->atributo2; //instancia só acessa atributos e metodos não estaticos
-Exemplo::metodo1();
+		public function metodo2() {
+			echo 'Eu sou um método normal';
+		}
+	}
 
-?>
+	//$x = new Exemplo();
+	/*
+	echo Exemplo::$atributo1;
+	echo '<br />';
+	Exemplo::metodo1();
+	*/
+	echo Exemplo::metodo1();
+
+	//echo $x->atributo2;
