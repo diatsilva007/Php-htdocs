49a1a5f2cd0e23ed20fbc2feefaccf2b698f6909
diatsilva007 <diatsilva007@gmail.com>
2025-02-24 23:27:08 -0300

    refactor: Melhorar a formatação e a legibilidade das classes Cliente e CadastroInterface nos namespaces A e B feat: Adicionar novo arquivo namespaces_parte2.php para demonstração de uso das classes Cliente

--- php_oo/namespaces_parte1.php
+++ php_oo/namespaces_parte1.php
@@ -1,80 +1,66 @@
 <?php
 
-namespace A;
+	namespace A;
 
-class Cliente implements \B\CadastroInterface
-{
-	public $nome = 'Jorge';
+	class Cliente implements \B\CadastroInterface {
+		public $nome = 'Jorge';
 
-	public function __construct()
-	{
-		echo '<pre>';
-		print_r(get_class_methods($this));
-		echo '</pre>';
+		public function __construct() {
+			echo '<pre>';
+			print_r(get_class_methods($this));
+			echo '</pre>';
+		}
+
+		public function __get($attr) {
+			return $this->$attr;
+		}
+
+		public function salvar() {
+			echo 'Salvar';
+		}
+
+		public function remover() {
+			echo 'Remover';
+		}
 	}
 
-	public function __get($attr)
-	{
-		return $this->$attr;
+	interface CadastroInterface {
+		public function salvar();
 	}
 
-	public function salvar()
-	{
-		echo 'Salvar';
+
+
+	namespace B;
+
+	class Cliente implements \A\CadastroInterface {
+		public $nome = 'Jamilton';
+
+		public function __construct() {
+			echo '<pre>';
+			print_r(get_class_methods($this));
+			echo '</pre>';
+		}
+
+		public function __get($attr) {
+			return $this->$attr;
+		}
+
+		public function remover() {
+			echo 'Remover';
+		}
+
+		public function salvar() {
+			echo 'Salvar';
+		}
 	}
 
-	public function remover()
-	{
-		echo 'Remover';
-	}
-}
-
-interface CadastroInterface
-{
-	public function salvar();
-}
-
-
-
-namespace B;
-
-class Cliente implements \A\CadastroInterface
-{
-	public $nome = 'Jamilton';
-
-	public function __construct()
-	{
-		echo '<pre>';
-		print_r(get_class_methods($this));
-		echo '</pre>';
+	interface CadastroInterface {
+		public function remover();
 	}
 
-	public function __get($attr)
-	{
-		return $this->$attr;
-	}
 
-	public function remover()
-	{
-		echo 'Remover';
-	}
+	//---------------
 
-	public function salvar()
-	{
-		echo 'Salvar';
-	}
-}
-
-interface CadastroInterface
-{
-	public function remover();
-}
-
-
-//---------------
-
-$c = new \B\Cliente();
-print_r($c);
-echo $c->__get('nome');
-
-?>
+	$c = new \B\Cliente();
+	print_r($c);
+	echo $c->__get('nome');
