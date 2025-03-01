f9a4b90b2a6a1bea7ebd140084b0efe6cb4a7abf
diatsilva007 <diatsilva007@gmail.com>
2025-02-28 23:47:11 -0300

    feat: Adicionar exemplo de comparações sensatas em PHP 8

---
+++ PHP 8 - features/comparacoes_sensatas.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '0 == "0": ' . (0 == "0"); // true | true
+echo "<br>";
+echo '0 == "0.0": ' . (0 == "0.0"); // true | true
+echo "<br>";
+echo '0 == "teste": ' . (0 == "teste"); // true | false
+echo "<br>";
+echo '0 == "": ' . (0 == ""); // true | false
+echo "<br>";
+echo '0 == "   50": ' . (50 == "   50"); // true | true
+echo "<br>";
+echo '0 == "50teste": ' . (50 == "50teste"); // true | false
+echo "<br>";
+
+?>
