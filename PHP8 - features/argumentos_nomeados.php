6de50c1901ff19acd9a52dd7fe6aab6857b9d082
diatsilva007 <diatsilva007@gmail.com>
2025-02-28 21:54:05 -0300

    feat: Adicionar função sendMail com parâmetros nomeados para envio de e-mails

---
+++ PHP8 - features/argumentos_nomeados.php
@@ -0,0 +1,11 @@
+<?php
+
+function sendMail($destinatarios = "", $cc = "", $assunto = "", $mensagem = "")
+{
+    echo "Destinatários: $destinatarios <br>";
+    echo "CC: $cc <br>";
+    echo "Assunto: $assunto <br>";
+    echo "Mensagem: $mensagem <br>";
+}
+
+?>
