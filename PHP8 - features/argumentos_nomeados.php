1db13178e3b411b82661c74072541dc8335bcd63
diatsilva007 <diatsilva007@gmail.com>
2025-02-28 22:03:58 -0300

    feat: Adicionar exemplos de uso da função sendMail com argumentos nomeados

--- PHP8 - features/argumentos_nomeados.php
+++ PHP8 - features/argumentos_nomeados.php
@@ -8,4 +8,29 @@ function sendMail($destinatarios = "", $cc = "", $assunto = "", $mensagem = "")
     echo "Mensagem: $mensagem <br>";
 }
 
+sendMail(
+    assunto: "Argumentos nomeados",
+    destinatarios: "diogo.ataidee@gmail.com",
+    mensagem: "Dominando a feature de argumentos nomeados do PHP 8",
+);
+
+echo "<hr>";
+
+// convencional -> respeitando a ordem dos parametros
+
+sendMail(
+    "diogo.ataidee@gmail.com",
+    "teste@teste.com",
+    "Argumentos nomeados",
+    "Dominando a feature de argumentos nomeados do PHP 8",
+);
+
+echo "<hr>";
+
+sendMail(
+    "diogo.ataidee@gmail.com",
+    "Argumentos nomeados",
+    "Dominando a feature de argumentos nomeados do PHP 8",
+)
+
 ?>
