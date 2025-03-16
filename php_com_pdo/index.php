b666eed64a97b56490832f98fbada0c150c47e64
diatsilva007 <diatsilva007@gmail.com>
2025-03-15 22:03:20 -0300

    refactor: Melhorar a estrutura e a legibilidade do código na conexão PDO

--- php_com_pdo/index.php
+++ php_com_pdo/index.php
@@ -1,13 +1,13 @@
 <?php
 
-    $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
-    $usuario = 'root';
-    $senha = '';
+$dsn = 'mysql:host=localhost;dbname=php_com_pdo';
+$usuario = 'root';
+$senha = '';
 
-    try{
-        $conexao = new PDO($dsn, $usuario , $senha);
-        
-        $query = '
+try {
+    $conexao = new PDO($dsn, $usuario, $senha);
+
+    $query = '
             create table tb_usuarios(
                     id int not null primary key auto_increment,
                     nome varchar (50) not null,
@@ -15,28 +15,28 @@
                     senha varchar(32) not null
                 )
         ';
-        $retorno = $conexao->exec($query); //retorno 0, pois nao estamos modificando registros
+    $retorno = $conexao->exec($query); //retorno 0, pois nao estamos modificando registros
 
-        echo $retorno;
+    echo $retorno;
 
-        // $query = '
-        //         insert into tb_usuarios(
-        //             nome, email, senha 
-        //         ) values (
-        //             "Jorge Sant Ana", "jorge@teste.com.br", "123456"
-        //         )
-        // ';
-        $query = '
+    // $query = '
+    //         insert into tb_usuarios(
+    //             nome, email, senha 
+    //         ) values (
+    //             "Jorge Sant Ana", "jorge@teste.com.br", "123456"
+    //         )
+    // ';
+    $query = '
                 delete from tb_usuarios
         ';
 
-        $retorno = $conexao->exec($query); //vai retornar 1
-        echo $retorno;
-    } catch(PDOException $e) {
-        // echo '<pre>';
-        //     print_r($e);
-        // echo '</pre>';
+    $retorno = $conexao->exec($query); //vai retornar 1
+    echo $retorno;
+} catch (PDOException $e) {
+    // echo '<pre>';
+    //     print_r($e);
+    // echo '</pre>';
 
-        echo 'Erro: '. $e->getCode(). ' Mensagem: '. $e->getMessage();
-        //registrar o erro de alguma forma.
-    }
+    echo 'Erro: ' . $e->getCode() . ' Mensagem: ' . $e->getMessage();
+    //registrar o erro de alguma forma.
+}
