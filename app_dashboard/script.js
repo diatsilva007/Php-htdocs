74f8677261f5c22223bad81a8976647f42428c3d
diatsilva007 <diatsilva007@gmail.com>
2025-04-01 23:12:10 -0300

    feat: Adicionar funcionalidade de carregamento dinâmico para as páginas de documentação e suporte

--- app_dashboard/script.js
+++ app_dashboard/script.js
@@ -1,1 +1,27 @@
-$(document).ready(() => {});
+$(document).ready(() => {
+  $("#documentacao").on("click", () => {
+    //$('#pagina').load('documentacao.html')
+
+    /*
+		$.get('documentacao.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+    $.post("documentacao.html", (data) => {
+      $("#pagina").html(data);
+    });
+  });
+
+  $("#suporte").on("click", () => {
+    //$('#pagina').load('suporte.html')
+
+    /*
+		$.get('suporte.html', data => { 
+			$('#pagina').html(data)
+		})
+		*/
+    $.post("suporte.html", (data) => {
+      $("#pagina").html(data);
+    });
+  });
+});
